0233bfc65ef5c9053e67b3586c570f93a4f3ffb0
Eisuke Kozu <eisuke@c-styles.jp>
2015-12-29 18:37:26 +0900

    rsync除外オプションを追加
    一緒に改行を入れた

--- shells/prepare_vagrant_workspace.sh
+++ shells/prepare_vagrant_workspace.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
 mkdir -p $2
-rsync -av --delete --exclude='.git' --exclude='.vagrant' --exclude='/vendor' --exclude='/Gemfile.lock' --exclude='.idea' $1 $2 || exit $?
+rsync -av --delete \
+        --exclude='.git' \
+        --exclude='.vagrant' \
+        --exclude='/vendor' \
+        --exclude='/packer_cache' \
+        --exclude='/cookbooks' \
+        --exclude='/Gemfile.lock' \
+        --exclude='.idea' \
+        $1 $2 || exit $?
